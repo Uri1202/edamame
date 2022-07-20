cd39b224b7c756999d9a3abc3401dca464e50e32
nyan92015 <75907477+nyan92015@users.noreply.github.com>
2022-07-21 00:42:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const baseUrl = "https://us-central1-aizuhack-353413.cloudfunctions.net";
+
 function Get_Profile() {
-    return fetch(baseUrl + "/Data?collection=teacher", {
-      method: "GET"
-    }).then((response) => {
+  return fetch(baseUrl + "/Data?collection=teacher", {
+    method: "GET"
+  })
+    .then((response) => {
       return response.json();
+    })
+    .then((data) => {
+      console.log(data);
     });
 }
-Get_Profile()
+Get_Profile();
