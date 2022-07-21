c4546d39fe77edd22452198e1fde23667a8565f0
nyan92015 <75907477+nyan92015@users.noreply.github.com>
2022-07-21 22:36:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,75 @@
-
 const baseUrl = "https://us-central1-aizuhack-353413.cloudfunctions.net";
 
+function CreateIcon(profileData) {
+  const iconData = document.createElement("a");
+  iconData.href = "profile.html?id=" + profileData.id;
 
-function CreateIcon(userData) {
+  //名前、画像、星評価のタグ作り
+  const iconImg = document.createElement("img");
+  iconImg.classname = "iconImg";
+  iconImg.src = profileData.icon_data.imageUrl;
 
-  const IconData = document.createElement("a");
-  IconData.href = "profile.html?id=" + userData.user.id;
+  const iconName = document.createElement("p");
+  iconName.classname = "profileName";
+  iconName.textContent = profileData.icon_data.name;
 
-  const userImg = document.createElement("img");
-  userImg.classname = "userImg";
-  userImg.src = userData.user.iconUrl;
+  const iconRate = document.createElement("div");
+  iconRate.classname = "profileRate";
 
-  const userName = document.createElement("p");
-  userName.classname = "userName";
-  userName.textContent = userData.user.displayName;
+  //星のタグ作り
+  const stars = {};
 
-  const userAssess = document.createElement("div");
-  userAssess.classname = "userAssess";
-  userAssess.textContent = userData.user.assessment;
+  for (let i = 1; i <= 5; i++) {
 
-  IconData.append(userImg, userName,userAssess);
-  return IconData;
+    const star = document.createElement("div");
+    stars["star" + String(i)] = star;
 
+    if (i <= Math.round(profileData.icon_data.rateAverage)) {
+      stars["star" + String(i)].classname = "yellow";
+    } else {
+      stars["star" + String(i)].classname = "grey";
+    }
+
+    iconRate.append(stars["star" + String(i)]);
+  }
+
+  iconData.append(iconImg, iconName, iconRate);
+  return iconData;
 }
-
 
 //テスト用のpost　↓
 
 function Post() {
-
-  const postTemplate = {
-    user: {
-      displayName: "nyan92015",
-      iconUrl: "https://twitter.com/nyan84392441/photo",
-      id: "1",
-    }
+  const profile_data = {
+    icon_data: {
+      name: "nyan92015",
+      imageUrl: "https://twitter.com/nyan84392441/photo",
+      rateAverage: 2.3
+    },
+    ratedCount: 1,
+    subjects: "数学",
+    comments: "おはよう"
   };
   fetch(baseUrl + "/Data?collection=teacher?", {
     method: "POST",
-    body: JSON.stringify(postTemplate)
+    body: JSON.stringify(profile_data)
   });
-
 }
 
+Post();
 
 function Get_Profile() {
-
   return fetch(baseUrl + "/Data?collection=teacher", {
     method: "GET"
   }).then((response) => {
     return response.json();
   });
-
 }
 
 const IconList = document.getElementsByClassName("main-content")[0];
 
-Get_Profile().then((IconsData) => {
-
-  for (let i = 0; i < IconsData.length; i = i + 1) {
-    IconList.append(CreateIcon(IconsData[i]));
+Get_Profile().then((iconsData) => {
+  for (let i = 0; i < iconsData.length; i = i + 1) {
+    IconList.append(CreateIcon(iconsData[i]));
   }
-
 });
